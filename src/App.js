79bef9d19f7e094793ab81377724d74412d06d2a
Your Name <you@example.com>
2022-03-11 14:42:09 +0100

    use borde api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import './App.css';
 import data from './data.json';
 
@@ -7,11 +7,34 @@ import Header from './Header';
 import TaskList from './TaskList';
 
 function App() {
-  const [ taskList, setTaskList ] = useState(data);
+  const [taskList] = useState(data);
+  const [dataAPI, setData] = useState(null);
+
+  const fetchData = () => {
+    return fetch('https://www.boredapi.com/api/activity/')
+    .then((response) => response.json())
+    .then((dataAPI) => {
+      setData([dataAPI])
+    })
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <TaskList taskList={taskList} />
+      <h1>THIS API IS CONTROLLING MY LIFE</h1>
+
+      {dataAPI &&
+        dataAPI.map(({activity}) => (
+          <div key={activity}>
+            <h3>{activity}</h3>
+          </div>
+        ))
+      }
     </div>
   );
 }
